284b8cea36691a82b7991b9fa70bcda653f679f9
Koydenjatke <97977463+Koydenjatke@users.noreply.github.com>
2024-08-03 02:25:04 +0300

    Complete 1.3

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -31,29 +31,27 @@ const Total = (props) => {
 };
 
 const App = () => {
-	const course = {
-		title: "Half Stack application development",
-		parts: [
-			{
-				name: "Fundamentals of React",
-				exercises: 10,
-			},
-			{
-				name: "Using props to pass data",
-				exercises: 7,
-			},
-			{
-				name: "State of a component",
-				exercises: 14,
-			},
-		],
+	const course = "Half Stack application development";
+	const part1 = {
+		name: "Fundamentals of React",
+		exercises: 10,
 	};
+	const part2 = {
+		name: "Using props to pass data",
+		exercises: 7,
+	};
+	const part3 = {
+		name: "State of a component",
+		exercises: 14,
+	};
+
+	const parts = [part1, part2, part3];
 
 	return (
 		<div>
 			<Header course={course} />
-			<Content parts={course.parts} />
-			<Total parts={course.parts} />
+			<Content parts={parts} />
+			<Total parts={parts} />
 		</div>
 	);
 };
